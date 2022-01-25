8026b85ea9f2304609050fb19db922b726a6c9de
lauretteroy <laurieroy.dev@gmail.com>
2022-01-25 15:13:56 -0800

    Scaffold notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_215709) do
+ActiveRecord::Schema.define(version: 2022_01_25_222426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2022_01_25_215709) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "notes", "users"
 end
